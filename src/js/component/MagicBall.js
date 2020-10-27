16072e5bd5492901b321b15452634d4e41ddf73d
MLopez8282 <saltairpix@gmail.com>
2020-10-27 22:39:11 +0000

    practice

---
+++ src/js/component/MagicBall.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export const MagicBall = () => {
+	let userQuestion = "Will I be a great web developer?";
+
+	let randomNumber = Math.floor(Math.random() * 8);
+
+	let eightBall = "";
+	switch (randomNumber) {
+		case 0:
+			eightBall = "It is certain";
+			break;
+		case 1:
+			eightBall = "It is decidely so";
+			break;
+		case 2:
+			eightBall = "Relpy hazy try again";
+			break;
+		case 3:
+			eightBall = "Cannot predict now";
+			break;
+		case 4:
+			eightBall = "Do not count on it";
+			break;
+		case 5:
+			eightBall = "My sources say no";
+			break;
+		case 6:
+			eightBall = "Outlook not so good";
+			break;
+		case 7:
+			eightBall = "Signs point to yes";
+			break;
+		default:
+			eightBall = "There is no answer";
+			break;
+	}
+	return (
+		<div>
+			<h1> {userQuestion} </h1>
+			<p> {eightBall}</p>
+		</div>
+	);
+};
